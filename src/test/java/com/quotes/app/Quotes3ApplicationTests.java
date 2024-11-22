2147925b0e87921b7417792f76dfade1a68d6ba9
3030932 <3030932@30309-32>
2024-11-22 13:25:44 +0900

    first comit

---
+++ src/test/java/com/quotes/app/Quotes3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quotes.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quotes3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
